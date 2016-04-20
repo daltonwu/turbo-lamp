9ec81f5aa0007080964366999c2c662646b4895f
Dalton Wu <dalton.z.wu@gmail.com>
2016-04-20 15:11:14 -0400

    :cancer:

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 /**
- * DW Softdev, Term 2, Spring 2016
+ * DW Softdev 20 April 2016
  * Shi Shu, Dalton Wu
  */
 
-function random_range(foo, bar) {
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+function rand_range(foo, bar) {
     /**
      * Returns random integer in interval [foo, bar)
      */
+    
     if(bar < foo) {
-        return random_range(bar, foo);
+        return rand_range(bar, foo);
     }
     
     var range = bar - foo;
@@ -16,7 +20,27 @@ function random_range(foo, bar) {
 }
 
 function Ball() {
-    this.radius = random_range(50, 100);
-    this.x = random_range(canvas.;
-    this.y = Math.floor(Math.random() *
+    this.r = rand_range(50, 100);
+    this.x = rand_range(0, canvas.width);
+    this.y = rand_range(0, canvas.width);
+    this.v_x = rand_range(10, 50);
+    this.v_y = rand_range(10, 50);
+    
+    // random color (hex)
+    this.color = "#";
+    for(i = 0; i < 3; i++) {
+        this.color += rand_range(0, 256).toString(16);
+    }
+    
+    this.draw = function {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 0.5;
+        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+        ctx.fill();
+    };
+}
+
+function toggleMovement() {
 }
